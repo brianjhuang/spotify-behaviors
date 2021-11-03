eb9917df67548eec65d67274684b6610282ac353
aimohapatra <aishanim@Aishanis-MacBook-Pro.local>
2021-11-02 18:22:08 -0700

    app edits

--- about_us_app.py
+++ about_us_app.py
@@ -9,9 +9,20 @@ st.set_page_config(layout="wide")
 #Title of Page
 st.markdown("""## __About Our Team__
 
-### Hello! We are undergraduate students at the University of California - San Diego
+### Hello! We are undergraduate data science students at the University of California - San Diego
 	""")
 
 
-images = ["images.png", "images.png", "images.png", "images.png"]
-st.image(images, use_column_width=True, caption=["Brian", "Annie", "Victor", "Aishani" ])
+images = ["spotify_streamlit_photos/brian.jpg", "spotify_streamlit_photos/annie.jpg", "spotify_streamlit_photos/victor.jpg",
+"spotify_streamlit_photos/aishani.jpg"]
+#st.image(images, width=300, caption=["Brian H", "Annie Fan", "Victor Thai", "Aishani Mohapatra" ])
+brian, annie, victor, aishani = st.columns(4)
+with brian:
+	brian = st.image(images[0], caption = "Brian Huang")
+with annie:
+	annie = st.image(images[1], caption = "Annie Fan")
+with victor:
+	victor = st.image(images[2], caption = "Victor Thai")
+with aishani:
+	aishani = st.image(images[3], caption = "Aishani Mohapatra")
+#st.image(images, use_column_width = False, caption=["Brian", "Annie", "Victor", "Aishani" ])
--- problems_app.py
+++ problems_app.py
@@ -7,15 +7,18 @@ import os
 st.set_page_config(layout="wide")
 
 #Title of Page
-st.markdown("# Discussion")
+st.markdown("# Discussion of Methods")
 
 
 #problems with sklearn
-st.markdown("""## __Problems with Sklearn__
+st.markdown("""## __Experiences with scikit-learn__	""")
 
+st.image("spotify_streamlit_photos/sklearn.png")
+
+st.markdown("""
 Using sklearn to generate a model that would predict skipping behavior allowed for
 data from different users to become a general prediction. In the real world, this
-is not accurate as every listener has their own unique taste and skipping beahvior.
+is not accurate as every listener has their own unique taste and skipping behavior.
 In other words, we simply cannot assume that everyone shares the same behavior through
 sampling from the whole dataset.
 
@@ -25,13 +28,22 @@ in order to determine traits that shape the skipping behavior of listeners.
 	""")
 
 #problems with behavior class
-st.markdown("""## __Problems with the Behavior Class__
+st.markdown("""## __Problems with the Behavior Class__""")
 
+st.image("spotify_streamlit_photos/clustering.png")
+
+st.markdown("""
 The behavior class is the prediction model we created from scratch; we had much more
 freedom to create our own models based on the specifications of what we wanted to do.
-However, we still came across some issues, especially since we had no starting framework
-and had to start everything from scratch.
+The main aspect we wanted to change from our sklearn model, was the fact that we
+weren't able to group users' listening data together while predicting if a user
+would skip a song. The prediction was generalized to all users in the dataset.
+
+However, we still came across some issues with our behavior class, especially since
+we had no starting framework and there were too many elements to account for that
+were much more easily done using the sklearn model, which is eventually what
+we used for our final model.
 
 	""")
 
-st.button("images.png")
+st.button("spotify_streamlit_photos/sklearn.png")
