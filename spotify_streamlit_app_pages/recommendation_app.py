4cc9a524d6e6deeb9e5110d4ec927e3943c812af
brianjhuang <brianjhuang25@gmail.com>
2021-12-14 04:09:49 -0600

    recommendation_app fixes

--- spotify_streamlit_app_pages/recommendation_app.py
+++ spotify_streamlit_app_pages/recommendation_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from multipage_template import save, MultiPage, start_app, clear_cache
 import pandas as pd
 from PIL import Image
 import os
@@ -11,6 +10,7 @@ import math
 from spotifyAPI import Spotify
 import json
 
+global s
 spotify_image_left, spotify_image_right = st.columns([1,8])
 
 with spotify_image_left:
@@ -20,16 +20,7 @@ st.markdown("""
 	# Recommendation Demo
 	""")
 
-st.write("For our recommendation model, we have generated it using cosine similarity. \
-	We first took in user data and grouped songs that they listened to. Using a similarity function, \
-	we used this function to train a system that takes in a group of songs and its features to \
-	return songs that these users are similar to the songs that they have already listened to. ")
 
-st.markdown("""
-	## Similarity Function
-
-	To determine the relative similarities that each song has with our prediction set, we used the following similarity function.
-	""")
 
 userOneFeatures = pd.read_csv("../userOneFeatures.csv")
 userTwoFeatures = pd.read_csv("../userTwoFeatures.csv")
@@ -39,114 +30,215 @@ userOneFeatures.drop('Unnamed: 0', axis = 1, inplace = True)
 userTwoFeatures.drop('Unnamed: 0', axis = 1, inplace = True)
 userThreeFeatures.drop('Unnamed: 0', axis = 1, inplace = True)
 
-update = st.button("Get the most recent songs?")
+update = st.button("Wanna change the songs? Hit this!")
 #the top 50 songs is always changing, so this lets us fetch new songs
 
+valid = False
+
 if update:
-	os.remove('features.json')
-	os.remove('songs.txt')
+	if os.path.exists('X_features.json'):
+		os.remove('X_features.json')
+	if os.path.exists('X_songs.txt'):
+		os.remove('X_songs.txt')
+	if os.path.exists('y_features.json'):
+		os.remove('y_features.json')
+	if os.path.exists('y_songs.txt'):
+		os.remove('y_songs.txt')
 
-def auth(client_id, client_secret):
-    s = Spotify(client_id, client_secret)
-    return s
 
-if not (os.path.exists('features.json') and os.path.exists('songs.txt')):
-    user = st.text_input("Client ID: ")
-    password = st.text_input("Client Secret: ")
-    s = auth(user, password)
-st.write('Authentication Passed')
+user = st.empty()
+u = user.text_input("Enter client ID: ")
+if u == "":
+	user = st.empty()
+password = st.empty()
+p = password.text_input("Enter client secret: ")
+if p == "":
+	password = st.empty()
+if len(u) > 0 and len(p) > 0:
+	s = Spotify(u, p)
+	if s.perform_auth():
+		st.write('Authentication Passed')
+		valid = True
+	else:
+		st.write('Authentication Failed')
+		valid = False
 
-st.write('Getting the Top 50 - USA...')
 
-@st.cache
-def get_songs():
-    if os.path.exists('songs.txt'):
-        songs = open('songs.txt').read().split(",")
-        if len(songs) <= 0:
-            songs = s.get_playlist_items()
-            with open('songs.txt', 'w', encoding = 'utf-8') as s_file:
-                for song in songs:
-                    s_file.write(song + ",")
-                songs = open('songs.txt').read().split(",")
-            return songs
-        return songs
-    else:
-        songs = s.get_playlist_items()
-        with open('songs.txt', 'w', encoding = 'utf-8') as s_file:
-            for song in songs:
-                s_file.write(song + ",")
-        songs = open('songs.txt').read().split(",")
-        return songs
+if valid:
+	def get_X(playlist = 'Top 50 - USA', creator = 'Spotify', playlist_id = ""):
+		'''
+		Takes in a playlis,m ;l909t name and the creator of the playlist and
+		returns the list of songs in that playlist. Also returns the features.
 
-@st.cache
-def run_model(data, songs):
-    if os.path.exists('features.json'):
-        f = open('features.json')
-        features = json.load(f)
-        if len(features) <= 0:
-            features = s.get_playlist_features('Top 50 - USA')
-            with open('features.json', 'w', encoding='utf-8') as f:
-                json.dump(features, f, ensure_ascii=False, indent=4)
-        model = songRecommender(data = userDict[userDown], predict = features, songs = songs)
-    else:
-        features = s.get_playlist_features('Top 50 - USA')
-        with open('features.json', 'w', encoding='utf-8') as f:
-            json.dump(features, f, ensure_ascii=False, indent=4)
-        model = songRecommender(data = userDict[userDown], predict = features, songs = songs)
-    return model
+		Params:
+		playlist (string) - the name of the playlist (default Top 50)
+		creator (string) - the creator of the playlist (default Spotify)
 
-songs = get_songs()
-st.write("Top 50 Songs Retrieved!")
-songOutput = st.selectbox('What song would you like to view?', tuple(songs))
+		Returns:
+		songs (list) - a list of song names
+		X (list of dictionaries) - our features
+		'''
+		if os.path.exists('X_songs.txt'):
+			#if our song exists
+			songs = open('X_songs.txt').read().split("~")
+			if len(songs) <= 1:
+				songs = s.get_playlist_items(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+				with open('X_songs.txt', 'w', encoding = 'utf-8') as s_file:
+					for song in songs:
+						s_file.write(song[0] + "~")
+					songs = open('X_songs.txt').read().split("~")
+		else:
+			songs = s.get_playlist_items(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+			with open('X_songs.txt', 'w', encoding = 'utf-8') as s_file:
+				for song in songs:
+					s_file.write(song[0] + "~")
+				songs = open('X_songs.txt').read().split("~")
 
-from songRecommender import songRecommender
+		if os.path.exists('X_features.json'):
+			f = open('X_features.json')
+			features = json.load(f)
+			if len(features) <= 1:
+				features = s.get_playlist_features(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+				with open('X_features.json', 'w', encoding='utf-8') as f:
+					json.dump(features, f, ensure_ascii=False, indent=4)
+		else:
+			features = s.get_playlist_features(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+			with open('X_features.json', 'w', encoding='utf-8') as f:
+				json.dump(features, f, ensure_ascii=False, indent=4)
 
-userTuple = ('One', 'Two', "Three")
+		return songs[:len(songs)-1], features
 
-userDown = st.selectbox('What user would you like to pick?', userTuple)
-userDict = {'One': userOneFeatures, 'Two': userTwoFeatures, 'Three': userThreeFeatures}
+	def get_y(playlist, creator, playlist_id):
+		'''
+		Takes in a playlist name and the creator of the playlist and
+		returns the list of songs in that playlist. Also returns the features.
 
-#plug in the user we want, the song we want
-model = run_model(userDict[userDown], songs)
-modelOutput = model.similar(model.getPredict(), model.getFeatures())
+		Params:
+		playlist (string) - the name of the playlist
+		creator (string) - the creator of the playlist
 
-output = modelOutput[songOutput]
-track_uri = output[0]
-simAndSong = output[1][0]
+		Returns:
+		songs (list) - a list of song names
+		features (list of dictionaries) - our y features
+		'''
+		if os.path.exists('y_songs.txt'):
+			#if our song exists
+			songs = open('y_songs.txt').read().split("~")
+			if len(songs) <= 1:
+				songs = s.get_playlist_items(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+				with open('y_songs.txt', 'w', encoding = 'utf-8') as s_file:
+					for song in songs:
+						s_file.write(song[0] + "~")
+					songs = open('y_songs.txt').read().split("~")
+		else:
+			songs = s.get_playlist_items(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+			with open('y_songs.txt', 'w', encoding = 'utf-8') as s_file:
+				for song in songs:
+					s_file.write(song[0] + "~")
+				songs = open('y_songs.txt').read().split("~")
 
-uri = f'<iframe src="https://open.spotify.com/embed/track/{track_uri[14:]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="autoplayt; encrypted-media"></iframe>'
-html_string ='''
-<script language = 'javascript'>
-    function autoplay() {
-        var t = setTimeout(function(){
-            var button = document.querySelector('[title="Play"]') || false;
-            if (button) {
-                console.log('Click', button)
-                button.click()
-            }
-        }, 999)
-    }
-    document.addEventListener('DOMContentLoaded', (event) => {
-        autoplay()
-    })
-</script>
-'''
+		if os.path.exists('y_features.json'):
+			f = open('y_features.json')
+			features = json.load(f)
+			if len(features) <= 1:
+				features = s.get_playlist_features(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+				with open('y_features.json', 'w', encoding='utf-8') as f:
+					json.dump(features, f, ensure_ascii=False, indent=4)
+		else:
+			features = s.get_playlist_features(query = playlist, desired_artist = creator, playlist_id = playlist_id)
+			with open('y_features.json', 'w', encoding='utf-8') as f:
+				json.dump(features, f, ensure_ascii=False, indent=4)
 
-st.write("The song you chose:")
-st.components.v1.html(uri + html_string, width=None, height=None, scrolling=False)
+		return songs[:len(songs)-1], features
+	playlist_valid = False
+	playlist = st.text_input("What playlist do you want? Can't decide? Type Top 50 for the Top 50! ")
+	creator = st.text_input("Who made the playlist? Can't decide? Type Spotify for the Top 50!")
+	playlistId = st.text_input('Can\'t find the name? Use your ID! Find the ID in your Spotify Account! ')
+	if (len(playlist) > 0 and len(creator) > 0) or (playlist == 'Top 50' and creator == 'Spotify'):
+		st.write('Getting your songs...')
+		playlist_valid = True
+	if playlist_valid:
+		X_songs, X = get_X(playlist, creator, playlistId)
+		while len(X_songs) <= 1:
+			X_songs, X = get_X(playlist, creator, playlistId)
+		st.write("Songs retrieved!")
 
-st.write("Here is the most simliar song from User " + str(userDown) + ": ", str(simAndSong[1]))
-st.write("Here is our similarity score:", str(simAndSong[0]))
+	playlist_valid = False
 
-st.markdown('#')
-st.markdown('#')
+	playlist = st.text_input("What playlist do you want? ")
+	creator = st.text_input("Who made the playlist? ")
+	playlistId = st.text_input('Can\'t find the name? Use your ID! ')
+	if len(playlist) > 0 and len(creator) > 0:
+		playlist_valid = True
+	if playlist_valid:
+		y_songs, y = get_y(playlist, creator, playlistId)
+		st.write('Getting your songs..')
+		while len(y_songs) <= 1:
+			y_songs, y = get_y(playlist, creator, playlistId)
+		st.write("Your songs have been retrieved!")
 
-bar_leftspacer, music_bar_left, music_bar, music_bar_right, bar_rightspacer = st.columns([10,1.5,1.5,1.5,10])
+		songOutput = st.selectbox('What song would you like to view?', tuple(y_songs))
 
-with music_bar:
-	play_button = st.image("../spotify_streamlit_photos/spotify_play_button.png")
-	# if play_button:
-	# 	play_button = st.image("pause_button.png")
-with music_bar_right:
-	st.image("../spotify_streamlit_photos/skip_button_spotify.png", use_column_width = True)
-st.progress(85)
+		from recommender import songRecommender
+
+		#plug in the user we want, the song we want
+		model = songRecommender(X, y, X_songs, y_songs)
+		modelOutput = model.similar(model.getX(), model.getY())
+
+		output = modelOutput[songOutput]
+		track_uri = output[0]
+		songRecs = output[1]
+
+		uri = f'<iframe src="https://open.spotify.com/embed/track/{track_uri[14:]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="autoplayt; encrypted-media"></iframe>'
+		html_string ='''
+		<script language = 'javascript'>
+			function autoplay() {
+				var t = setTimeout(function(){
+					var button = document.querySelector('[title="Play"]') || false;
+					if (button) {
+						console.log('Click', button)
+						button.click()
+					}
+				}, 999)
+			}
+			document.addEventListener('DOMContentLoaded', (event) => {
+				autoplay()
+			})
+		</script>
+		'''
+
+		st.write("The song you chose:")
+		st.components.v1.html(uri + html_string, width=None, height=None, scrolling=False)
+
+		st.write('The 5 most similiar songs: ')
+		sim_scores = []
+		uris = []
+		for rec in songRecs:
+			sim_scores.append(rec[0])
+			uris.append(rec[2])
+		st.write('Similarity Score: ' + str(sim_scores[0]))
+		st.components.v1.html(f'<iframe src="https://open.spotify.com/embed/track/{uris[0][14:]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="autoplayt; encrypted-media"></iframe>', width=None, height=None, scrolling=False)
+		st.write('Similarity Score: ' + str(sim_scores[1]))
+		st.components.v1.html(f'<iframe src="https://open.spotify.com/embed/track/{uris[1][14:]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="autoplayt; encrypted-media"></iframe>', width=None, height=None, scrolling=False)
+		st.write('Similarity Score: ' + str(sim_scores[2]))
+		st.components.v1.html(f'<iframe src="https://open.spotify.com/embed/track/{uris[2][14:]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="autoplayt; encrypted-media"></iframe>', width=None, height=None, scrolling=False)
+		st.write('Similarity Score: ' + str(sim_scores[3]))
+		st.components.v1.html(f'<iframe src="https://open.spotify.com/embed/track/{uris[3][14:]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="autoplayt; encrypted-media"></iframe>', width=None, height=None, scrolling=False)
+		st.write('Similarity Score: ' + str(sim_scores[4]))
+		st.components.v1.html(f'<iframe src="https://open.spotify.com/embed/track/{uris[4][14:]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="autoplayt; encrypted-media"></iframe>', width=None, height=None, scrolling=False)
+
+		st.markdown('#')
+		st.markdown('#')
+
+		bar_leftspacer, music_bar_left, music_bar, music_bar_right, bar_rightspacer = st.columns([10,1.5,1.5,1.5,10])
+
+		with music_bar:
+			play_button = st.image("../spotify_streamlit_photos/spotify_play_button.png")
+			# if play_button:
+			# 	play_button = st.image("pause_button.png")
+		with music_bar_right:
+			st.image("../spotify_streamlit_photos/skip_button_spotify.png", use_column_width = True)
+		st.progress(85)
+
+		with music_bar_left:
+			st.image("../spotify_streamlit_photos/back_button_spotify.png", use_column_width = True)
